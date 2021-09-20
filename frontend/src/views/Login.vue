91d1ad88e1c058fb564a610e89962fd465603c51
Hyungoo Kang <oognuyh@gmail.com>
2021-09-20 13:21:18 +0900

    refactor: add api url to environment variables

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,78 +2,81 @@
   <v-container
     fill-height
   >
-      <v-row
-        align="center"
-        justify="center"
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+        lg="4"
       >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-            lg="4"
+        <v-card
+          elevation="0"
+        >
+          <a
+            href="/"
           >
-            <v-card
-              elevation="0"
+            <v-img 
+              src="@/assets/logo.png"
+              alt="logo"
+              contain
+              height="100"
+            />
+          </a>
+          <v-card-title
+            class="justify-center"
+          >
+            Welcome to Kanban Board
+          </v-card-title>
+          <v-card-text>
+            <v-btn
+              :href="getAuthorizationUrl('github')"
+              dark
+              tile
+              x-large
+              block
             >
-              <a
-                href="/"
+              <v-icon
+                large
+                left
+                class="mr-5"
               >
-                <v-img 
-                  src="@/assets/logo.png"
-                  alt="logo"
-                  contain
-                  height="100"
-                />
-              </a>
-              <v-card-title
-                class="justify-center"
+                mdi-github
+              </v-icon>
+              Login with Github
+            </v-btn>
+            <v-btn
+              :href="getAuthorizationUrl('google')"
+              x-large
+              block
+              tile
+              class="mt-3"
+            >
+              <v-icon
+                large
+                left
+                class="mr-5"
               >
-                Welcome to Kanban Board
-              </v-card-title>
-
-              <v-card-text>
-                <v-btn
-                  href="http://localhost:9999/oauth2/authorization/github"
-                  dark
-                  tile
-                  x-large
-                  block
-                >
-                  <v-icon
-                    large
-                    left
-                    class="mr-5"
-                  >
-                    mdi-github
-                  </v-icon>
-                  Login with Github
-                </v-btn>
-
-                <v-btn
-                  href="http://localhost:9999/oauth2/authorization/google"
-                  x-large
-                  block
-                  tile
-                  class="mt-3"
-                >
-                  <v-icon
-                    large
-                    left
-                    class="mr-5"
-                  >
-                    mdi-google
-                  </v-icon>
-                  Login with Google
-                </v-btn>
-              </v-card-text>
-            </v-card>
-          </v-col>
-      </v-row>
+                mdi-google
+              </v-icon>
+              Login with Google
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
+    methods: {
+        getAuthorizationUrl(registrationId) {
+            return `${process.env.VUE_APP_API_URL}/oauth2/authorization/${registrationId}`
+        }
+    }
 }
 </script>
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    port: 9998,
-    proxy: {
-      "/api": {
-        target: "http://localhost:9999/"
-      }
+    transpileDependencies: [
+        'vuetify'
+    ],
+    devServer: {
+        port: 9998,
+        proxy: {
+            "/api": {
+                target: process.env.VUE_APP_API_URL
+            }
+        }
     }
-  }
 }
